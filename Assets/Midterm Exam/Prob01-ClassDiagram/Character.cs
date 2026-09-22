f2b8e074545bc1b844287de7dd6035898ab3a3d5
stxrlight2208-blip <stxrlight2208@gmail.com>
2026-09-22 13:11:54 +0700

    2

--- Assets/Midterm Exam/Prob01-ClassDiagram/Character.cs
+++ Assets/Midterm Exam/Prob01-ClassDiagram/Character.cs
@@ -4,6 +4,10 @@ namespace MidtermExam.Prob01
 {
     public class Character
     {
+        string characterName;
+        float moveSpeed;
+        int level;
+
         // TODO: Implement inheritance, fields and methods according to Class Diagram
     }
 }
